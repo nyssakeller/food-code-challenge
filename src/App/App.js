37017c87664d060a5f644b2e18db39c05e782918
Nyssa Keller <nyssa1@gmail.com>
2018-10-15 15:51:40 -0600

    Create addFoodToFavorites method. Pass this method to AddFoodForm as props

--- src/App/App.js
+++ src/App/App.js
@@ -3,10 +3,22 @@ import AddFoodForm from '../AddFoodForm/AddFoodForm';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favoriteFoodArray: []
+    }
+  }
+
+  addFoodToFavorites = food => {
+    const { favoriteFoodArray } = this.state;
+    this.setState({ favoriteFoodArray: [...favoriteFoodArray, food] });
+  }
+
   render() {
     return (
       <div className="App">
-        <AddFoodForm />
+        <AddFoodForm addFoodToFavorite={ this.addFoodToFavorites }/>
       </div>
     );
   }
