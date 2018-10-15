2487458f915c571b255245b3dad2954c640ca424
Nyssa Keller <nyssa1@gmail.com>
2018-10-15 15:37:43 -0600

    Create base of AddFoodForm component

---
+++ src/AddFoodForm/AddFoodForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AddFoodForm extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default AddFoodForm;
